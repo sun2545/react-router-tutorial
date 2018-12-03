780ceccd6af5561ca7b6e68e81d396c7fd07710b
Sunhye Kwon <kwonsunhye0206@gmail.com>
2018-12-02 19:25:52 -0500

    router when come back /home

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About =()=>{
+    return(
+        <div>
+            <h2>introduce</h2>
+            <p>
+                Hello, I am react router
+                </p>
+                </div>
+    );
+};
+
+export default About;
